d2a1f16e7c44cb8bd7ee612588597a6bd9a81eae
John Campbell <johnmarcampbell@gmail.com>
2017-02-06 02:50:23 -0500

    Remove days_until_midterms() function

--- main.py
+++ main.py
@@ -1,17 +1,9 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
-import arrow
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
-def days_until_midterms(date=arrow.utcnow()):
-    """Given a date, this function will tell you how many days
-    it will be before the next midterm election."""
-    election_day = arrow.get('2018-11-06', 'YYYY-MM-DD')
-    delta = election_day - date
-    return delta.days
-    
 @app.route('/')
 def index():
     return render_template('index.html')
